b9eafa77471b5c60715be76e0b5f80648919f86f
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-02-24 19:06:34 +0100

    add helpers to test dummy app

--- test/dummy/app/helpers/application_helper.rb
+++ test/dummy/app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def beautify html_string
+    HtmlBeautifier.beautify html_string.strip.gsub('>', ">\n").gsub('<', "\n<")
+  end
+
+  def render_source args={}
+    #@html_encoder ||= HTMLEntities.new
+    #raw(@html_encoder.encode(render args))
+
+    partial_file_path = "app/views/forms/_#{args[:partial]}.html.erb"
+    partial_file_content = File.read(partial_file_path)
+
+    beautify partial_file_content.strip.gsub('>', ">\n").gsub('<', "\n<")
+  end
+
+  def render_out args={}
+    #@html_encoder ||= HTMLEntities.new
+    #HtmlBeautifier.beautify(raw(@html_encoder.encode(render args).html_safe.strip.gsub('>', ">\n").gsub('<', "\n<"))).html_safe
+
+    beautify((render args).strip.gsub('>', ">\n").gsub('<', "\n<"))
+  end
 end
